be8a0d52760cd10344d768722ffc39774379be4c
vasiliskkk <157796320+vasiliskkk@users.noreply.github.com>
2024-10-11 15:03:29 +0300

    Добавьте файлы проекта.

---
+++ 11_10_Task6/11_10_Task6.cpp
@@ -0,0 +1,27 @@
+﻿//6.Утановка курсора в новую позицию
+#include <windows.h> 
+#include <iostream> 
+using namespace std;
+int main()
+{
+	char c;
+	HANDLE hStdout; // дескриптор стандартного вывода 
+	COORD coord; // для позиции курсора
+	// читаем дескриптор стандартного вывода 
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Input new cursor position." << endl;
+	cout << "X = ";
+	cin >> coord.X;
+	cout << "Y = ";
+	cin >> coord.Y;
+	// установить курсор в новую позицию
+	if (!SetConsoleCursorPosition(hStdout, coord))
+	{
+		cout << "Set cursor position failed." << endl;
+		return GetLastError();
+	}
+	cout << "This is a new position." << endl;
+	cout << "Input any char to exit: ";
+	cin >> c;
+	return 0;
+}
